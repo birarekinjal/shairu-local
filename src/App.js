8969e1afb62ee61a240eb7a237535d5a4d0266e1
birarekinjal <kinjal.birare@tntra.io>
2022-08-22 15:42:02 +0530

    [Add] Done Eslint

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 import { PersistGate } from "redux-persist/integration/react";
-import { Provider, useDispatch, useSelector } from "react-redux";
+import { Provider } from "react-redux";
+import logo from "./logo.svg";
 import store, { persistor } from "./setup/store";
-import AuthenticationDemo from './modules/authentication/authentication-demo';
+import AuthenticationDemo from "./modules/authentication/authentication-demo";
 // import { simpleAction } from './modules/authentication/redux/authenticationAction';
 
-
 function App() {
-
   // const dispatch = useDispatch();
   // const state = useSelector((state) => state);
 
@@ -22,7 +20,6 @@ function App() {
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <div className="App">
-
           {/* //<button onClick={handleClick}> Check the Redux </button> */}
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
@@ -42,9 +39,6 @@ function App() {
         </div>
       </PersistGate>
     </Provider>
-
-
-
   );
 }
 
