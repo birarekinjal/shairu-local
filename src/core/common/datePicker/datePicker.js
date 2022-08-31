cee97bd55c133f0c108fb1b94944e836c62197e0
birarekinjal <kinjal.birare@tntra.io>
2022-08-31 14:31:35 +0530

    working on common components

--- src/core/common/datePicker/datePicker.js
+++ src/core/common/datePicker/datePicker.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { DatePicker, Space } from 'antd';
+import { DatePicker } from 'antd';
 import moment from 'moment';
 
-const CustomDatePicker = (props) => {
-  const { RangePicker } = DatePicker;
+function CustomDatePicker(props) {
+  const { RangePicker, TimePicker } = DatePicker;
   const {
     onChange,
     showTime,
     picker,
     format,
-    type,                            //type - date | range  | time
+    type, // type - date | range  | time
     size,
     status,
-    defaultValue,                        //ex - {[moment('2015/01/01', dateFormat), moment('2015/01/01', dateFormat)]
+    defaultValue, // ex - {[moment('2015/01/01', dateFormat), moment('2015/01/01', dateFormat)]
     disabled,
     value,
     renderExtraFooter,
@@ -21,37 +21,39 @@ const CustomDatePicker = (props) => {
   } = props;
 
   const customProps = {
-    onChange: onChange,
-    picker: picker,                 // week | month |quarter |year
-    showTime: showTime,
-    format: format,                       //ex - YYYY-MM-DD HH:mm
-    onOk: onOk,
-    size: size,
-    status: status,
-    placement: placement || "bottomRight",    // topLeft |topRight |bottomLeft |bottomRight
-    defaultValue: defaultValue,
-    disabled: disabled,
-    value: value,
-    renderExtraFooter: renderExtraFooter
-  }
+    onChange,
+    picker, // week | month |quarter |year
+    showTime,
+    format, // ex - YYYY-MM-DD HH:mm
+    onOk,
+    size,
+    status,
+    placement: placement || "bottomRight", // topLeft |topRight |bottomLeft |bottomRight
+    defaultValue,
+    disabled,
+    value,
+    renderExtraFooter
+  };
 
   if (type === "date") {
-    return <DatePicker {...customProps} />
+    return <DatePicker {...customProps} />;
   }
   if (type === "range") {
-    return <RangePicker
-      {...customProps}
-      ranges={{
-        Today: [moment(), moment()],
-        'This Month': [moment().startOf('month'), moment().endOf('month')],
-      }}
-    />
+    return (
+      <RangePicker
+        {...customProps}
+        ranges={{
+          Today: [moment(), moment()],
+          'This Month': [moment().startOf('month'), moment().endOf('month')],
+        }}
+      />
+    );
   }
 
   if (type === " time") {
     return <TimePicker {...customProps} />;
   }
 
-};
+}
 
 export default CustomDatePicker;
